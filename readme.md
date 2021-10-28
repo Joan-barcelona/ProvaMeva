aa59b2037abfc56a76f55461a82630a9e93e9a59
Joan Besora <jebesora@hotmail.com>
2021-10-28 10:29:03 +0200

    initial commit meu

---
+++ readme.md
@@ -0,0 +1,13 @@
+
+# Proves Git
+
+## Conectar repositori Git a eclipse
+
+la url del repositori:
+
+https://github.com/Joan-barcelona/ProvaMeva
+
+
+segueixo video:
+
+https://www.youtube.com/watch?v=8bjtf6TZZGA
---
+++ src/provesGit/ProvarGit.java
@@ -0,0 +1,10 @@
+package provesGit;
+
+public class ProvarGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Provo Git i Github amb eclipse.");
+	}
+
+}
